0e4abaa237976df1d084ccc94c4dc6144be4567e
Akino <93914105+CirnoDayo@users.noreply.github.com>
2024-04-04 14:43:25 +0700

    NavMesh WIP

--- Assets/Prototype3/Scripts/Akino/Akino_CameraMovement.cs
+++ Assets/Prototype3/Scripts/Akino/Akino_CameraMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class Akino_CameraMovement : MonoBehaviour
 {
-    [Range(1f, 5f)] public float sensitivity = 1f;
+    public float minZoom = 5f;
+    public float maxZoom = 50f;
+    [Range(0f, 10f)] public float zoomSensitivity = 1f;
 
     Vector3 currentPosition;
     Vector3 movementVector;
@@ -15,19 +17,24 @@ public class Akino_CameraMovement : MonoBehaviour
         currentPosition = transform.position;
     }
 
-    private void LateUpdate()
+    private void Update()
     {
         movementVector.x = Input.GetAxis("Mouse X");
         movementVector.z = Input.GetAxis("Mouse Y");
         movementVector = Quaternion.Euler(0f, 45f, 0f) * movementVector;
         if (Input.GetMouseButton(1))
         {
-            transform.position -= movementVector * sensitivity;
+            transform.position += movementVector;
             Cursor.lockState = CursorLockMode.Locked;
         }
         else
         {
             Cursor.lockState = CursorLockMode.None;
         }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float size = Camera.main.orthographicSize - scroll * zoomSensitivity;
+        size = Mathf.Clamp(size, minZoom, maxZoom);
+        Camera.main.orthographicSize = size;
     }
 }
