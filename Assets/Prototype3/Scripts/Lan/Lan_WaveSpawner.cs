addeaae3e18abea8a56057cd7f2c36ac2e76ef70
HoangLanlam <hoanglan8140304@gmail.com>
2024-04-12 16:33:22 +0700

    enemy spawn count by wave

--- Assets/Prototype3/Scripts/Lan/Lan_WaveSpawner.cs
+++ Assets/Prototype3/Scripts/Lan/Lan_WaveSpawner.cs
@@ -11,12 +11,16 @@ public class Lan_WaveSpawner : MonoBehaviour
     public Transform enemyPrefab;
     public Vector3 spawnPoint;
     public Button startButton;
+    public Transform enemyWithBomb;
+
+    [SerializeField] private int numberOfEnemiesInFirstWave = 10;
 
     [Header("Attributes")] 
     [SerializeField] float delayInstantiateTime = 0f;
     //public float timeBetweenWaves = 5f;
     //public float countdown = 2f;
     [SerializeField] int waveIndex = 0;
+    [SerializeField] private float enemyDefaultInstantiatedRate = .2f;
    
 
     [Header("Private")]
@@ -54,23 +58,47 @@ public class Lan_WaveSpawner : MonoBehaviour
     public void SpawnWaveInput()
     {
         StartCoroutine(SpawnWave());
+       
     }
+    
+    private float numberOfEnemiesLastWave = 0;
+    private float numberOfEnemiesThisWave;
 
     IEnumerator SpawnWave()
     {
+        Debug.Log("SpawnWave method is calling!");
+        numberOfEnemiesThisWave = numberOfEnemiesInFirstWave;
         waveIndex++;
+        int totalEnemyInstantiated = 0;
+        Debug.Log("waveIndex: " + waveIndex);
+
+        for (int i = 1; i < waveIndex; i++)
+        {
+            numberOfEnemiesThisWave = numberOfEnemiesLastWave + (numberOfEnemiesLastWave * enemyDefaultInstantiatedRate);
+            Debug.Log("For loop for add more enemy is working!");
+        }
         
-        //Debug.Log("Wave Incoming!");
-        for (int i = 0; i < waveIndex; i++)
+        Debug.Log("Original numberOfEnemies: " + numberOfEnemiesThisWave);
+        Debug.Log("Round to Int= " + Mathf.RoundToInt(numberOfEnemiesThisWave));
+        totalEnemyInstantiated = (int)Mathf.RoundToInt(numberOfEnemiesThisWave);
+        
+        for (int i = 0; i < totalEnemyInstantiated; i++)
         {
             yield return new WaitForSeconds(delayInstantiateTime);
+            
             SpawnEnemy();
             
         }
+
+        numberOfEnemiesLastWave = numberOfEnemiesThisWave;
+        Debug.Log(numberOfEnemiesLastWave*enemyDefaultInstantiatedRate);
+
+
+
     }
     void SpawnEnemy()
     {
-        
+        //Debug.Log("SpawnEnemy is calling!");
         Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
     }
 }
